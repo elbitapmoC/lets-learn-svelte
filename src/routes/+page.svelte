d9a8b545e41c4ed770d3bc0c0870313a7d76df03
Ahkal <bp@sojourn.llc>
2024-09-22 17:05:12 -0400

    save progress

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,37 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	// imports
+	import Heading from './components/Heading.svelte';
+	import Counter from './components/Counter.svelte';
+	import ToDo from './components/ToDo.svelte';
+
+	let name = '🍽️ Ahkal';
+	let substring = 'We hope you <strong>love</strong> your time here with us.';
+	let src = 'https://i.giphy.com/uVgqFuSbAQ15tV1NsK.webp';
+	let alt = 'NEVER GIVE UP!';
+</script>
+
+<header>
+	<aside>
+		<Heading />
+		<!-- @html - does NOT sanitize. -->
+		<p>{@html substring}</p>
+		<Counter />
+	</aside>
+	<img {src} alt="{name} fights and fights, preaching {alt}" />
+</header>
+
+<ToDo />
+
+<style>
+	header {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		background-color: rgb(249, 237, 216);
+		padding: 1rem;
+		gap: 1rem;
+	}
+	p {
+		font-style: italic;
+	}
+</style>
---
+++ src/routes/components/Heading.svelte
@@ -0,0 +1,9 @@
+<p>Welcome to Svelte/SvelteKit</p>
+
+<style>
+	p {
+		font-size: 2rem;
+		font-weight: bold;
+		margin: 0;
+	}
+</style>
